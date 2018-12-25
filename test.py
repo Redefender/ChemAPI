6e361d860466282548e6825751d3ff5780f2521e
Ezra Jesalva <ejesalv1@uncc.edu>
2018-12-25 09:15:59 -0500

    add firebase sign in

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-test_answer = [
-    {"This is NOT an answer, just to fix off by one errors"},
-    {"answer": "this is the answer for question 1"},
-    {"answer": "this is the answer for question 2"},
-    {"answer": "this is the answer for question 3"},
-    {"answer": "this is the answer for question 4"},
-    {"answer": "this is the answer for question 5"},
-    {"answer": "this is the answer for question 6"},
+import pyrebase
 
-]
+config = {
+    "apiKey": "AIzaSyAnqidILVhAGjxg5Ya3AJhWtmGR5RvJCMY",
+    "authDomain": "chemdata-a814d.firebaseapp.com",
+    "databaseURL": "https://chemdata-a814d.firebaseio.com",
+    "projectId": "chemdata-a814d",
+    "storageBucket": "chemdata-a814d.appspot.com",
+    "messagingSenderId": "219386075120"
+}
 
-print(test_answer[2]['answer'])
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+email = input('Please enter your email\n')
+password = input('Please enter your password\n')
+
+# create new user
+# user = auth.create_user_with_email_and_password(email, password)
+
+# sign in
+auth.sign_in_with_email_and_password(email, password)
