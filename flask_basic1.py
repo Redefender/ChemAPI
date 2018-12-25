85eb68801245d1fbb753e1a09541013ded55ba23
Ezra Jesalva <ejesalv1@uncc.edu>
2018-12-25 08:43:26 -0500

    Add virtual environment and delete Crypto to fix pyrebase issues

--- flask_basic1.py
+++ flask_basic1.py
@@ -1,7 +1,19 @@
 from flask import Flask, jsonify, request
 from collections import defaultdict
+from flask_cors import CORS
+import pyrebase
+
+config = {
+    "apikey": "AIzaSyAnqidILVhAGjxg5Ya3AJhWtmGR5RvJCMY",
+    "authDomain": "chemdata-a814d.firebaseapp.com",
+    "databaseURL": "https://chemdata-a814d.firebaseio.com",
+    "projectId": "chemdata-a814d",
+    "storageBucket": "chemdata-a814d.appspot.com",
+    "messagingSenderId": "219386075120"
+}
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
@@ -11,34 +23,43 @@ def hello():
 
 @app.route('/lab-one', methods=['GET', 'POST'])
 def answer_one():
-    questions = request.json['questions']  # ex ['1','5','7']
+    # if request.method == 'POST':
+    # questions = request.json['questions']  # ex ['1','5','7']
 
     # convert all strings within question list to ints
-    questions = map(int, questions)
+    # questions = map(int, questions)
 
     # get the answers for each question (also a dictionary of lists) probably going to use mongodb or firebase
     test_answer = [
-                   "This is NOT an answer, just to fix off by one errors",
-                   "this is the answer for question 1",
-                   "this is the answer for question 2",
-                   "this is the answer for question 3",
-                   "this is the answer for question 4",
-                   "this is the answer for question 5",
-                   "this is the answer for question 6",
-                   "this is the answer for question 7",
-                   "this is the answer for question 8",
-                   "this is the answer for question 9",
-                   "this is the answer for question 10"
-                   ]
+        {"This is NOT an answer, just to fix off by one errors"},
+        {"answer": "this is the answer for question 1"},
+        {"answer": "this is the answer for question 2"},
+        {"answer": "this is the answer for question 3"},
+        {"answer": "this is the answer for question 4"},
+        {"answer": "this is the answer for question 5"},
+        {"answer": "this is the answer for question 6"},
+        {"answer": "this is the answer for question 7"},
+
+    ]
 
     # create a dictionary of lists
     response_dictionary = defaultdict(list)
 
-    for i in questions:
-        response_dictionary[i].append(test_answer[i])
+    # for i in questions:
+    #     response_dictionary[i].append(test_answer[i])
 
-    return jsonify(response_dictionary)
-
+    return jsonify({"lab": "Lab One", "answers": [
+        {
+            "answer": "2",
+            "identifier": ".",
+            "value": "question"
+        },
+        {
+            "answer": "3",
+            "identifier": ".",
+            "value": "question"
+        }
+    ]})
 
 if __name__ == "__main__":
     app.run()
---
+++ test.py
@@ -0,0 +1,12 @@
+test_answer = [
+    {"This is NOT an answer, just to fix off by one errors"},
+    {"answer": "this is the answer for question 1"},
+    {"answer": "this is the answer for question 2"},
+    {"answer": "this is the answer for question 3"},
+    {"answer": "this is the answer for question 4"},
+    {"answer": "this is the answer for question 5"},
+    {"answer": "this is the answer for question 6"},
+
+]
+
+print(test_answer[2]['answer'])
