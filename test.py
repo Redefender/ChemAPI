8c94307d57e31f167cb1f201b856aec855577c62
Ezra Jesalva <ejesalv1@uncc.edu>
2018-12-25 12:21:30 -0500

    FINALLY have admin authentication working with test.py

--- test.py
+++ test.py
@@ -1,22 +1,24 @@
 import pyrebase
+import firebase_admin
+from firebase import firebase
+from firebase_admin import credentials
+from firebase_admin import db
 
-config = {
+
+# THIS is the magic. This local private file is what allows me to read and write to database
+cred = credentials.Certificate("/users/ezraj/OneDrive/Documents/firebase/secret.json")
+
+firebase_admin.initialize_app(cred, {
     "apiKey": "AIzaSyAnqidILVhAGjxg5Ya3AJhWtmGR5RvJCMY",
     "authDomain": "chemdata-a814d.firebaseapp.com",
     "databaseURL": "https://chemdata-a814d.firebaseio.com",
     "projectId": "chemdata-a814d",
     "storageBucket": "chemdata-a814d.appspot.com",
     "messagingSenderId": "219386075120"
-}
+})
 
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+# auth = firebase.auth()
+ref = db.reference('boi').set("this is a new test with admin")
 
-email = input('Please enter your email\n')
-password = input('Please enter your password\n')
 
-# create new user
-# user = auth.create_user_with_email_and_password(email, password)
 
-# sign in
-auth.sign_in_with_email_and_password(email, password)
