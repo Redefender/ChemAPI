58eb5cdd4c32824caa9e2693aab76fe9ca847893
Ezra Jesalva <ezra.jesalva15@gmail.com>
2018-12-28 21:54:04 -0500

    Implement Firebase Authentication with verified email user

--- flask_basic1.py
+++ flask_basic1.py
@@ -1,13 +1,23 @@
 from flask import Flask, jsonify, request
 import firebase_admin
+import pyrebase
 from flask_cors import CORS
 from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import auth
 from RequestData import RequestData
 
 # THIS is the magic. This local private file is what allows me to read and write to database
 cred = credentials.Certificate("/users/ezraj/OneDrive/Documents/firebase/secret.json")
 
+config = {
+    "apiKey": "AIzaSyAnqidILVhAGjxg5Ya3AJhWtmGR5RvJCMY",
+    "authDomain": "chemdata-a814d.firebaseapp.com",
+    "databaseURL": "https://chemdata-a814d.firebaseio.com",
+    "projectId": "chemdata-a814d",
+    "storageBucket": "chemdata-a814d.appspot.com",
+    "messagingSenderId": "219386075120"
+}
 firebase_admin.initialize_app(cred, {
     "apiKey": "AIzaSyAnqidILVhAGjxg5Ya3AJhWtmGR5RvJCMY",
     "authDomain": "chemdata-a814d.firebaseapp.com",
@@ -17,8 +27,18 @@ firebase_admin.initialize_app(cred, {
     "messagingSenderId": "219386075120"
 })
 
+# this is a flask app
 app = Flask(__name__)
+
+#This will accept cors
 CORS(app)
+
+#for regulate login user
+firebase = pyrebase.initialize_app(config)
+user_db = firebase.database()
+my_auth = firebase.auth()
+
+token = ''
 
 
 @app.route("/")
@@ -26,22 +46,39 @@ def hello():
     return jsonify({"about": "Hello World!"})
 
 
-@app.route('/lab-one', methods=['GET', 'POST'])
-def answer_one():
-
+@app.route('/sign-in', methods=['GET', 'POST'])
+def sign_in():
     data = ''
     if request.method == 'POST':
-        return 'it is a post'
         data = request.get_json()
+    user = my_auth.sign_in_with_email_and_password('thiswillgetfilled',
+                                                   'thiswillgetfilled')  # need to use firebase.auth()
+    global token
+    token = user['idToken']
+    return 'signed in?'
+
+
+@app.route('/lab-one', methods=['GET', 'POST'])
+def answer_one():
+    data = ''
+
+    print(token)
+    decoded_token = auth.verify_id_token(token)
+    isPost = False
+    if request.method == 'POST':
+        isPost = True
+        # data = jsonify(request.get_json())
+        root = db.reference()
+        authenticated = db.reference("test").get()
 
     # parse into object
-    request_data = RequestData("one", "two", "3")
+    # request_data = RequestData(data.data["lab"], "2", "3")
 
     # request proper lab
-    root = db.reference()
-    query_data = jsonify(root.child("labs").child("1").get())
+    lab_root = user_db.child("labs")
+    # query_data = jsonify(lab_root.child(request_data.lab).get())
 
-    return data
+    return jsonify(authenticated)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -5,18 +5,45 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 
-class Employee:
+# class Employee:
+#
+#     def __init__(self, first, last, pay):
+#         self.first = first
+#         self.last = last
+#         self.pay = pay
+#
+#     def full_name(self):
+#         return '{} {}'.format(self.first, self.last)
+#
+#
+# emp_1 = Employee('Ezra', 'Jesalva', 50000)
+# emp_2 = Employee('Corey','Shaefer', 65000)
+# print(emp_2.full_name())
+# print('{} is loaded through statically'.format(Employee.full_name(emp_1)))
+#
+# for i in range(10):
+#     print(i)
 
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
-
-    def full_name(self):
-        return '{} {}'.format(self.first, self.last)
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
 
-emp_1 = Employee('Ezra', 'Jesalva', 50000)
-emp_2 = Employee('Corey','Shaefer', 65000)
-print(emp_2.full_name())
-print('{} is loaded through statically'.format(Employee.full_name(emp_1)))
+class LinkedList:
+    def __init__(self):
+        self.head = Node()
+
+    def append(self, data):
+        new_node = Node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+
+    def length(self):
+        cur = self.head
+        length = 0
+        while cur.next != None:
+            cur = cur.next
+            length += 1
